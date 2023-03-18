6bc8d462b7dafd63a7e30971df5cece128118e5f
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-03-18 20:48:14 +0100

    solved task 9

--- 9-print_comb.c
+++ 9-print_comb.c
@@ -5,17 +5,21 @@ int main()
     int a, b;
 
 
-    for (a = '0'; a < '9'; a++)
+    for (a = 0; a < 9; a++)
     {
-        for (b = '0' + 1; b <= '9'; b++)
+        for (b = a + 1; b <= 9; b++)
         {
-
-            putchar(a);
-            putchar(b);
+            putchar((a % 10) + '0');
+            putchar((b % 10) + '0');
+            if( a == 8 && b == 9)
+            {
+                continue;
+            }
             putchar(',');
             putchar(' ');
         }
     }
+    putchar('\n');
     return (0);
 
 }
