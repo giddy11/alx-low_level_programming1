2173faffc34b4de97a0bda26b1d14f6b3d41dcd4
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-03-21 20:27:47 +0100

    worked on the function nested loops

---
+++ alx-function-nested/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
+*/
+
+void print_alphabet(void)
+{
+    char i;
+
+    for ( i = 'a'; i <= 'z'; i++)
+    {
+        _putchar(i);
+    }
+    
+    
+}
