01dec49377fc7f524c6247a9e6d2f6b70fa8e201
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-06-05 12:06:08 +0100

    completed

--- NesoTutorial/stringLiterals/1_strcpy.c
+++ NesoTutorial/stringLiterals/1_strcpy.c
@@ -29,50 +29,51 @@ char *_strncpy(char *dest, char *src, int n)
     return dest;
 }
 
-int main(void)
-{
-    char s1[98];
-    char *ptr;
-    int i;
+// int main(void)
+// {
+//     char s1[98];
+//     char *ptr;
+//     int i;
 
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
-    return (0);
-}
+//     for (i = 0; i < 98 - 1; i++)
+//     {
+//         s1[i] = '*';
+//     }
+//     s1[i] = '\0';
+//     printf("%s\n", s1);
+//     ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+//     printf("%s\n", s1);
+//     printf("%s\n", ptr);
+//     ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+//     printf("%s", s1);
+//     printf("%s", ptr);
+//     for (i = 0; i < 98; i++)
+//     {
+//         if (i % 10)
+//         {
+//             printf(" ");
+//         }
+//         if (!(i % 10) && i)
+//         {
+//             printf("\n");
+//         }
+//         printf("0x%02x", s1[i]);
+//     }
+//     printf("\n");
+//     return (0);
+// }
 
-/*int main()
+int main()
 {
     char str1[12] = "Hello";
     char str2[4];
 
-    // char *str = strncpy(str2, str1, sizeof(str2));
-    // printf("%s\n", str);
+    char *str = strncpy(str2, str1, sizeof(str2));
+    printf("%s\n", str);
     _strncpy(str2, str1, sizeof(str2));
+    str2[sizeof(str2) - 1] = '\0';
 
     printf("%s\n", str2);
 
     return (0);
-}*/
+}
